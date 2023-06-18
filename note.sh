9e975b4a14d63155aa0fe3abdb9dae9825c163b1
Sekou-sDevOps <sdukuly00@gmail.com>
2023-06-18 08:46:35 -0400

    Im feeling bless

---
+++ note.sh
@@ -0,0 +1,82 @@
+SonarQube 2  
+Nexus 1/2 
+
+Jenkins integration with:
+   Git/Github  
+   maven  
+   Sonarqube  
+   nexus  
+   tomcat  
+   nginx webserver   
+
+Jenkins
+   Is an automation tool 
+   Is an open source continuous integration (CI) tool written in java.
+
+What is Jenkins automating ?
+  
+How do you use Jenkins for CI/CD automation ?
+In Landmark, we manage federated JAVA based web & enterprise applications (web.ear) 
+for Fintech Clients/ DOD/ E-commerce.
+FinTech = Financial Technology :
+
+Financial Technology can perform the following;
+1. payment Gateways   
+2. account creation 
+3. credit management 
+2. etc. 
+
+FinTech = Financial Technology can be use by any company.  
+
+POS Machine =   
+  Banks     = TD / RBC / BOA / cHASE / Barclays / UBA / Ecobank / BICEC
+  Insurance =  
+  Payment Gateways = paypal, visa, master, interac, 
+                     pushpay, cashapp, zelle, mobileMoney  
+  E-commerce = amazon, ebay, alibaba, 
+  Telecoms  = MTN, Vadofone, Glo, verizon, tmobile, at&t
+  Health care = NIS, NIH, 
+  Retail = Walmart, cotsco, 
+  Airlines = AA, UA, BA, KLM, AC
+  sports = FIFA, UEFA, CAF, FECAFOOT, NBA, UFC, ETC.
+  Governments = DOD, IRS, CRA, FIRS , 
+  betting = LOTTO, 1XBET,  
+  Oil & Gas = shell, total, BP, NNPC 
+  Crypto = coinbase, FTX
+  over 90% of companies uses FinTech today =
+  Taxi =  uber, Lyft 
+  Food chain = pizza, ubereats
+  IT Companies = AWS, AZURE, GCP  
+  auto = Tesla, Toyota, Mercedes, BMW, Honda, GMC, 
+
+
+What is the difference b/w github and nexus??   
+  - infrastructure difference 
+      - GITHUB is a SaaS tool
+      - Nexus is an IaaS tool ---          
+  - use cases:
+      - GITHUB IS A SCM  
+      - Nexus is an Artifactory 
+
+  - Vendor:
+      - GITHUB = Microsoft 
+      - Nexus  = Sonatype Nexus    
+
+Jenkins, is an open source Continuous Integration (ci/cd)
+tool written in Java. 
+Jenkins can be installed in Multiple Platforms:  
+  Linux 
+  Windows 
+  macOS 
+  Solaris 
+
+ansible  can be installed in Linux Platforms ONLY  : 
+Explain your EXPERIENCE in open source Technologies?? :
+  Linux OS, maven, tomcat, sonarQube, Nexus, Nginx, Git, GitHub  
+
+Explain you EXPERIENCE in applying Jenkins?? 
+ 1. I have installed and configure Multiple Jenkins servers 
+     https://github.com/LandmakTechnology/package-management
+     https://www.jenkins.io/doc/book/installing/   :
+      www.jenkins.io
+
